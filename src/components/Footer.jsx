fb8decfcce425bad391633092be8fc1342ecd6b7
j-preim <j.preimesberger@gmail.com>
2024-03-05 01:39:40 -0600

    added icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,30 @@
 export default function Footer() {
-
   return (
     <>
-      <p className="text-center">&copy; Joe Preimesberger 2024</p>
+      <div className="my-2 text-center">
+        <a
+          href="https://www.linkedin.com/in/joe-preimesberger-5962802b/"
+          target="_blank"
+        >
+          <i
+            className="me-3 fa-brands fa-linkedin fa-3x fa-fw icon"
+            id="linkedIn"
+          />
+        </a>
+        <a href="https://github.com/j-preim/" target="_blank">
+          <i
+            className="me-3 fa-brands fa-github fa-3x fa-fw icon"
+            id="gitHub"
+          />
+        </a>
+        <a href="https://www.salesforce.com/trailblazer/jpreimesberger" target="_blank">
+        <i
+          className="ms-1 fa-brands fa-salesforce fa-3x fa-fw icon"
+          id="salesforce"
+        />
+        </a>
+      </div>
+      <p className="my-2 text-center h6">&copy; Joe Preimesberger 2024</p>
     </>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,6 +7,24 @@ export default function Contact() {
   return (
     <>
     <h4>Contact Me</h4>
+    <section id="contact-me">
+        <article className="contact">
+          <ul>
+            <li><strong>Phone:&nbsp;</strong>612-386-3245</li>
+            <li><strong>Email:&nbsp;</strong>j.preimesberger@gmail.com</li>
+            <li>
+              <strong>GitHub:&nbsp;</strong>
+              <a href="https://github.com/j-preim/">My GitHub</a>
+            </li>
+            <li>
+              <strong>LinkedIn:&nbsp;</strong>
+              <a
+                href="https://www.linkedin.com/in/joseph-preimesberger-5962802b/"
+                >My LinkedIn</a>
+            </li>
+          </ul>
+        </article>
+      </section>
     </>
   )
 }
